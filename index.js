b63768fbaa87937f6423f1b1523a460b3450b319
Johnny Murillo <jossters@gmail.com>
2021-05-06 11:00:48 -0700

    lets see

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+require('dotenv').config()
+const path = require('path')
 const express = require('express')
-
 const server = express()
-
 const PORT = process.env.PORT || 5000
-
-process.env.API_KEY
-
+process.env.API_KEY || 'gaga'
+server.use(express.static(path.join(__dirname, 'client/build')))
 server.use(express.json())
-
 server.get('/api/*', (req, res) => {
-    res.json({
-        cohort: 'WEB 41',
-    })
+  res.json({
+    cohort: 'Web 41',
+  })
 })
-
-server.listen(PORT, ( => {
-    console.log(`listinig on ${PORT}`)
-}))
+server.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
